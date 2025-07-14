e2e584b5e6b299e664a2de78027aef62ef8dcd94
amitmamgai2k <123645217+amitmamgai2k@users.noreply.github.com>
2025-07-14 12:52:35 +0530

    Writing User Data Schema

---
+++ TravelBuddy/Backend/src/model/user.model.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+
+
+const UserSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  profilePicture: {
+    type: String,
+    default: 'https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?semt=ais_hybrid&w=740'
+  },
+
+  bio: {
+    type: String,
+    default: ''
+  },
+
+  currentLocation: {
+    lat: {
+      type: Number
+    },
+    lng: {
+      type: Number
+    }
+
+  },
+
+  futureDestinations: [
+    {
+      name: String,
+      lat: Number,
+      lng: Number,
+      startDate: Date,
+      endDate: Date
+    }
+  ],
+
+  interests: [String],
+
+  socialLinks: {
+    instagram: String,
+    facebook: String,
+    linkedin: String
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model('User', UserSchema);
+
+
